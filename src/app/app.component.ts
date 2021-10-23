4db80aaa847c6c4424821a039436519d72bc3230
rudith <ircc.199@gmail.com>
2021-10-23 17:05:16 -0500

    New front 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,11 @@
-import { Component } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'buro-client';
 
-  constructor(public auth: AuthService){}
-
-  loginWithRedirect() {
-    this.auth.loginWithRedirect();
-  }
-
-  logout(){
-    this.auth.logout();
-  }
 }
