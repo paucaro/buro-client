d0d472f712937b0333dfd0daba75509fa5739905
paucaro <carolinaasto@gmail.com>
2021-10-24 03:46:10 -0500

    Adding services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  host: '',
+  port: '',
+  dbhost: '',
+  dbport: '',
+  api_url: 'https://3l0mnuins5.execute-api.us-east-1.amazonaws.com/dev',
+  test: 'test',
+  socket_url: 'ws://3l0mnuins5.execute-api.us-east-1.amazonaws.com/dev'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  host: '',
+  port: '',
+  dbhost: '',
+  dbport: '',
+  api_url: 'http://__ELBURL__/',
+  test: 'test',
+  socket_url: 'ws://__ELBURL__'
 };
