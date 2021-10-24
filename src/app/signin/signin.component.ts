94a7c7936d1eddf1c7d7d09a2a02202dd8e2135c
paucaro <carolinaasto@gmail.com>
2021-10-24 03:09:01 -0500

    Adding login

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'signin',
@@ -7,17 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  email: string;
-  password: string;
+  userForm: FormGroup;
+  submitted = false;
+  error: string = null;
+  loading = false;
 
-  constructor() {}
-
-  login() {
-    console.log(this.email);
-    console.log(this.password);
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    ) {
   }
 
   ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      username: new FormControl('', {
+        validators: Validators.compose([Validators.required,
+        Validators.minLength(4), Validators.maxLength(20)]),
+        updateOn:'blur'
+      })
+    })
+  }
+
+  get user() { return this.userForm.controls; }
+
+  login() {
+    if (this.loading) {
+      return;
+    }
+    this.error = null;
+    this.submitted = true;
+    this.loading = true;
+    if (this.userForm.invalid) {
+      this.loading = false;
+      return;
+    }
+    const user = this.userForm.value;
+    console.log(user);
   }
 
 }
